f12b7a13f70a5aefd241d6ea27c4092a35109336
Mattis DALLEAU <mattisdalleau@gmail.com>
2020-06-09 18:54:42 +0400

    Updating launch.sh for MacOs and WSL Compatibility

--- launch.sh
+++ launch.sh
@@ -16,35 +16,14 @@ ask() {
     done
 }
 
-progress_bar()
-{
-    local PROG_BAR_MAX=${1:-30}
-    local PROG_BAR_DELAY=${2:-1}
-    local PROG_BAR_TODO=${3:-"."}
-    local PROG_BAR_DONE=${4:-"|"}
-    local i
-
-    echo -en "["
-    for i in `seq 1 $PROG_BAR_MAX`
-    do
-        echo -en "$PROG_BAR_TODO"
-    done
-    echo -en "]\0015["
-    for i in `seq 1 $PROG_BAR_MAX`
-    do
-        echo -en "$PROG_BAR_DONE"
-        sleep ${PROG_BAR_DELAY}
-    done
-    echo
-}
-
-if !ask "You are going to run the Radiant Bot please make sure you set your token before in /src/main.py"
+if ask "You are going to run the Radiant Bot please make sure you set your token before in /src/main.py"
 then
-    echo -e "\nOh too bad :( Please please come back soon :D !"
-    exit
+    echo -e "\nLet's gooooooooooooooooooo\n"
 fi
 
-progress_bar 10
-echo -e "\nLet's gooooooooooooooooooo\n"
+for i in {001..100}; do
+    sleep 1
+    printf "\r $i %"
+done
 
-python3 /src/main.py
+python3 src/main.py
